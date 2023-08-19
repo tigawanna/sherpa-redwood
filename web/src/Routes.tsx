a27daf0747843e2f8fd19a3ca0e969099148d773
tigawanna <denniskinuthiaw@gmail.com>
2023-08-19 17:03:03 +0300

    add supabase auth basics

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,26 +7,35 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 import DashboardLayout from './layouts/DashboardLayout/DashboardLayout'
 
 import { useAuth } from './auth'
+import AuthLayout from './layouts/AuthLayout/AuthLayout'
 
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Set wrap={ScaffoldLayout} title="UserProfiles" titleTo="userProfiles" buttonLabel="New UserProfile" buttonTo="newUserProfile">
-        <Route path="/user-profiles/new" page={UserProfileNewUserProfilePage} name="newUserProfile" />
-        <Route path="/user-profiles/{id:Int}/edit" page={UserProfileEditUserProfilePage} name="editUserProfile" />
-        <Route path="/user-profiles/{id:Int}" page={UserProfileUserProfilePage} name="userProfile" />
-        <Route path="/user-profiles" page={UserProfileUserProfilesPage} name="userProfiles" />
-      </Set>
       <Route path="/" page={HomePage} name="home" />
-      <Set path="/dashboard" wrap={DashboardLayout} name="root" >
-      <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+
+      <Private unauthenticated="home">
+        <Set wrap={ScaffoldLayout} title="UserProfiles" titleTo="userProfiles" buttonLabel="New UserProfile" buttonTo="newUserProfile">
+          <Route path="/user-profiles/new" page={UserProfileNewUserProfilePage} name="newUserProfile" />
+          <Route path="/user-profiles/{id:Int}/edit" page={UserProfileEditUserProfilePage} name="editUserProfile" />
+          <Route path="/user-profiles/{id:Int}" page={UserProfileUserProfilePage} name="userProfile" />
+          <Route path="/user-profiles" page={UserProfileUserProfilesPage} name="userProfiles" />
+        </Set>
+        <Set path="/dashboard" wrap={DashboardLayout} name="root">
+          <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+        </Set>
+      </Private>
+
+      <Set  wrap={AuthLayout} >
+       <Route path="/login" page={LoginPage} name="login" />
       </Set>
+
       <Route notfound page={NotFoundPage} />
     </Router>
   )
